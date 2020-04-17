c9bd8cf03c4c2ef85a4633311c9891459a0da5c1
Andre <andre.rian@inventsoftware.com.br>
2020-04-17 18:29:05 -0300

    Ajuste finais

--- Consultorio_Web/Controllers/AgendamentosController.cs
+++ Consultorio_Web/Controllers/AgendamentosController.cs
@@ -160,27 +160,5 @@ namespace Consultorio_Web.Controllers
             return RedirectToAction("Index");
 
         }
-
-        // GET: Agendamentos/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Agendamentos/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
